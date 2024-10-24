cbe8198b889a1e2596ac3da560f5c7a30e777980
farzeen-alii <techzeen10@gmail.com>
2024-10-24 21:31:36 +0500

    Firestore Database

--- app/welcome.jsx
+++ app/welcome.jsx
@@ -7,8 +7,11 @@ const Welcome = () => {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-3xl text-teal-900">Welcome</Text>
-      <TouchableOpacity className="bg-blue-500 py-3 px-10 rounded-lg shadow-md w-full" onPress={() => router.push('/realtime')}>
+      <TouchableOpacity className="bg-blue-500 py-3 px-10 rounded-lg shadow-md w-11/12" onPress={() => router.push('/realtime')}>
         <Text className="text-center text-white text-lg font-semibold">Explore Realtime Database</Text>
+      </TouchableOpacity>
+      <TouchableOpacity className="bg-blue-500 py-3 px-10 rounded-lg shadow-md w-11/12" onPress={() => router.push('/firestore')}>
+        <Text className="text-center text-white text-lg font-semibold">Explore Firestore Database</Text>
       </TouchableOpacity>
     </View>
   )
